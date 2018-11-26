d4ce9ef34edfa8ea89e25cfde89bfa7a96f9c178
Sarah Paz <sarah.paz@gmail.com>
2018-11-26 04:43:12 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -14,3 +14,29 @@ WIN_COMBINATIONS = [
   [0, 4, 8], # Top left to bottom right diagonal
   [2, 4, 6] # Top right to bottom left diagonal
 ]
+
+for each win_combination in WIN_COMBINATIONS
+  win_index_1 = win_combination[0]
+  win_index_2 = win_combination[1]
+  win_index_3 = win_combination[2]
+  win_index_4 = win_combination[3]
+  win_index_5 = win_combination[4]
+  win_index_6 = win_combination[5]
+  win_index_7 = win_combination[6]
+  win_index_8 = win_combination[7]
+
+  position_1 = board[win_index_1]
+  position_2 = board[win_index_2]
+  position_3 = board[win_index_3]
+  position_4 = board[win_index_4]
+  position_5 = board[win_index_5]
+  position_6 = board[win_index_6]
+  position_7 = board[win_index_7]
+  position_8 = board[win_index_8]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combination
+    else
+      false
+    end
+  end
