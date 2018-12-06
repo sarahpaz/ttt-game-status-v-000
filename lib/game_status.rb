efd4fb512f20a559ac2c05ce8aa77d87fff17819
Sarah Paz <sarah.paz@gmail.com>
2018-12-06 13:55:26 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,44 +1,44 @@
 # Helper Method
 def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
+    !(board[index].nil? || board[index] == " ")
 end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-  [0, 1, 2], # Top row
-  [3, 4, 5], # Middle row
-  [6, 7, 8], # Bottom row
-  [0, 3, 6], # First column
-  [1, 4, 7], # Second column
-  [2, 5, 8], # Third column
-  [0, 4, 8], # Top left to bottom right diagonal
-  [2, 4, 6] # Top right to bottom left diagonal
+    [0, 1, 2], # Top row
+    [3, 4, 5], # Middle row
+    [6, 7, 8], # Bottom row
+    [0, 3, 6], # First column
+    [1, 4, 7], # Second column
+    [2, 5, 8], # Third column
+    [0, 4, 8], # Top left to bottom right diagonal
+    [2, 4, 6] # Top right to bottom left diagonal
 ]
 
 def won?(board)
-  WIN_COMBINATIONS.detect do |win_combo|
-    board[win_combo[0]] == board[win_combo[1]] && board[win_combo[1]] == board[win_combo[2]] && position_taken?(board, win_combo[0])
-		end
+    WIN_COMBINATIONS.detect do |win_combination|
+      board[win_combination[0]] == board[win_combination[1]] && board[win_combination[0]] == board[win_combination[2]] && position_taken?(board, win_combination[0])
+    end
 end
 
 def full?(board)
-  board.all? do |space|
+    board.all? do |space|
     space == "X" || space == "O"
-  end
+    end
 end
 
 def draw?(board)
-  full?(board) && !won?(board)
+  !won?(board) && full?(board)
 end
 
 def over?(board)
-  won?(board) || full?(board) || draw?(board)
+    won?(board) || draw?(board) || full?(board)
 end
 
 def winner(board)
-  if win_combo = won?(board)
-    board[win_combo[0]]
-  else
-    return nil
+    if win_combination = won?(board)
+      board[win_combination[0]]
+    else
+      return nil
   end
 end
